d433bb01ccce2a0f6f848c353efe4f0fab038fa2
Simon Grimm <saimon@devdactic.com>
2024-01-24 09:48:04 +0100

    Add contacts modal.

--- app/(modals)/new-chat.tsx
+++ app/(modals)/new-chat.tsx
@@ -1,147 +1,80 @@
-import { View, Text, SectionList, StyleSheet, StatusBar } from 'react-native';
+import { View, Text, SectionList, StyleSheet, StatusBar, Image } from 'react-native';
 import contacts from '@/assets/data/contacts.json';
-import { useState } from 'react';
 import Colors from '@/constants/Colors';
+import { AlphabetList } from 'react-native-section-alphabet-list';
+import { defaultStyles } from '@/constants/Styles';
 
 const Page = () => {
-  // Define sections from A-Z with {title: 'A', data: [contacts.filter((contact) => contact.last_name[0] === 'A')]}
-  const sections = [
-    {
-      title: 'A',
-      data: contacts.filter((contact) => contact.last_name[0] === 'A'),
-    },
-    {
-      title: 'B',
-      data: contacts.filter((contact) => contact.last_name[0] === 'B'),
-    },
-    {
-      title: 'C',
-      data: contacts.filter((contact) => contact.last_name[0] === 'C'),
-    },
-    {
-      title: 'D',
-      data: contacts.filter((contact) => contact.last_name[0] === 'D'),
-    },
-    {
-      title: 'E',
-      data: contacts.filter((contact) => contact.last_name[0] === 'E'),
-    },
-    {
-      title: 'F',
-      data: contacts.filter((contact) => contact.last_name[0] === 'F'),
-    },
-    {
-      title: 'G',
-      data: contacts.filter((contact) => contact.last_name[0] === 'G'),
-    },
-    {
-      title: 'H',
-      data: contacts.filter((contact) => contact.last_name[0] === 'H'),
-    },
-    {
-      title: 'I',
-      data: contacts.filter((contact) => contact.last_name[0] === 'I'),
-    },
-    {
-      title: 'J',
-      data: contacts.filter((contact) => contact.last_name[0] === 'J'),
-    },
-    {
-      title: 'K',
-      data: contacts.filter((contact) => contact.last_name[0] === 'K'),
-    },
-    {
-      title: 'L',
-      data: contacts.filter((contact) => contact.last_name[0] === 'L'),
-    },
-    {
-      title: 'M',
-      data: contacts.filter((contact) => contact.last_name[0] === 'M'),
-    },
-    {
-      title: 'N',
-      data: contacts.filter((contact) => contact.last_name[0] === 'N'),
-    },
-    {
-      title: 'O',
-      data: contacts.filter((contact) => contact.last_name[0] === 'O'),
-    },
-    {
-      title: 'P',
-      data: contacts.filter((contact) => contact.last_name[0] === 'P'),
-    },
-    {
-      title: 'Q',
-      data: contacts.filter((contact) => contact.last_name[0] === 'Q'),
-    },
-    {
-      title: 'R',
-      data: contacts.filter((contact) => contact.last_name[0] === 'R'),
-    },
-    {
-      title: 'S',
-      data: contacts.filter((contact) => contact.last_name[0] === 'S'),
-    },
-    {
-      title: 'T',
-      data: contacts.filter((contact) => contact.last_name[0] === 'T'),
-    },
-    {
-      title: 'U',
-      data: contacts.filter((contact) => contact.last_name[0] === 'U'),
-    },
-    {
-      title: 'V',
-      data: contacts.filter((contact) => contact.last_name[0] === 'V'),
-    },
-    {
-      title: 'W',
-      data: contacts.filter((contact) => contact.last_name[0] === 'W'),
-    },
-    {
-      title: 'X',
-      data: contacts.filter((contact) => contact.last_name[0] === 'X'),
-    },
-    {
-      title: 'Y',
-      data: contacts.filter((contact) => contact.last_name[0] === 'Y'),
-    },
-    {
-      title: 'Z',
-      data: contacts.filter((contact) => contact.last_name[0] === 'Z'),
-    },
-  ];
-  console.log(sections[0].data);
+  const data = contacts.map((contact, index) => ({
+    value: `${contact.first_name} ${contact.last_name}`,
+    name: `${contact.first_name} ${contact.last_name}`,
+    img: contact.img,
+    desc: contact.desc,
+    key: `${contact.first_name} ${contact.last_name}-${index}`,
+  }));
 
   return (
-    <View style={{ flex: 1, backgroundColor: Colors.background }}>
-      <SectionList
-        sections={sections}
-        keyExtractor={(item, index) => item.last_name + index}
-        renderItem={({ item }) => (
-          <View style={styles.item}>
-            <Text style={styles.title}>{`${item.first_name} ${item.last_name}`}</Text>
+    <View style={{ flex: 1, paddingTop: 110, backgroundColor: Colors.background }}>
+      <AlphabetList
+        data={data}
+        stickySectionHeadersEnabled
+        indexLetterStyle={{
+          color: Colors.primary,
+          fontSize: 12,
+        }}
+        indexContainerStyle={{
+          width: 24,
+          backgroundColor: Colors.background,
+        }}
+        renderCustomItem={(item: any) => (
+          <>
+            <View style={styles.listItemContainer}>
+              <Image source={{ uri: item.img }} style={styles.listItemImage} />
+              <View>
+                <Text style={{ color: '#000', fontSize: 14 }}>{item.value}</Text>
+                <Text style={{ color: Colors.gray, fontSize: 12 }}>
+                  {item.desc.length > 40 ? `${item.desc.substring(0, 40)}...` : item.desc}
+                </Text>
+              </View>
+            </View>
+            <View style={[defaultStyles.separator, { marginLeft: 50 }]} />
+          </>
+        )}
+        renderCustomSectionHeader={(section) => (
+          <View style={styles.sectionHeaderContainer}>
+            <Text style={{ color: Colors.gray }}>{section.title}</Text>
           </View>
         )}
-        renderSectionHeader={({ section: { title } }) => <Text style={styles.header}>{title}</Text>}
-        style={{ marginTop: 120 }}
+        style={{
+          marginLeft: 14,
+        }}
       />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-  },
-  header: {
-    fontSize: 32,
+  listItemContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+    height: 50,
+    paddingHorizontal: 14,
     backgroundColor: '#fff',
   },
-  title: {
-    fontSize: 24,
+
+  listItemImage: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+  },
+
+  sectionHeaderContainer: {
+    height: 30,
+    backgroundColor: Colors.background,
+    justifyContent: 'center',
+    paddingHorizontal: 14,
   },
 });
 
